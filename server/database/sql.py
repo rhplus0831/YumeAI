38324c52e51b99fd546d57140bd73be8f63ef34e
RH+ <rhplus0831@proton.me>
2025-01-14 09:07:28 +0900

    Add Alembic migrations setup
    Set up Alembic for database migrations with an initial configuration, including environment setup, versioning, and template files. This initializes the basic framework required for managing database schema changes in the project.

--- server/database/sql.py
+++ server/database/sql.py
@@ -1,3 +1,7 @@
+import os
+
+from alembic import command
+from alembic.config import Config
 from sqlalchemy import create_engine
 from sqlmodel import SQLModel
 
@@ -7,11 +11,16 @@ import configure
 def get_engine(path: str = "_DEF_", create_meta: bool = True):
     if path == "_DEF_":
         path = configure.get_store_path("yumeAI.db")
+
     engine = create_engine(
         f"sqlite:///{path}", connect_args={"check_same_thread": False}
     )
 
-    if create_meta:
-        SQLModel.metadata.create_all(engine)
+    os.chdir(os.path.dirname(configure.__file__))
+    alembic_config = Config( os.path.join(os.path.dirname(configure.__file__), "alembic.ini") )
+    alembic_config.set_main_option("script_location", os.path.join(os.path.dirname(configure.__file__), "alembic") )
+    command.upgrade(alembic_config, "head")
+
+    print("데이터베이스 초기화 완료")
 
     return engine
